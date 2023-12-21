24d2c4983cfd58953b785fe542737346d6037ed9
Jeonghyeon Lee <jhlee.ryan@gmail.com>
2023-12-20 19:28:51 -0500

    create.php after posting go to index right away

--- create.php
+++ create.php
@@ -98,6 +98,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 		echo '<br><h6 style="text-align:center">' . $addResult . '</h6>';
         // Close connection
         $postDAO->getMysqli()->close();
+
+        echo "<script>location.href='index.php'</script>";
         }
 }
 
